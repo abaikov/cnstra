68bcdc3cb986e12005c76bb8f51319deab20bdb0
Andrey Baikov <bboyair@mail.ru>
2025-08-30 20:33:08 +0300

    Add typings

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['src/index.ts'],
     format: ['esm', 'cjs'],
-    dts: false,
+    dts: false, // Disable type generation for now
     splitting: false,
     sourcemap: true,
     clean: true,
